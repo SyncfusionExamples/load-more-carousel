d906a43074b001d50ad20ea8651b128a5cb0188c
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-26 12:23:54 +0530

    Load more sample has been added

---
+++ LoadMore/LoadMore/Model/CarouselModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LoadMore
+{
+    public class CarouselModel
+    {
+
+        public CarouselModel(string imagestr)
+        {
+            Image = imagestr;
+        }
+        private string _image;
+
+        public string Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+
+    }
+}
---
+++ LoadMore/LoadMore/ViewModel/CarouselViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LoadMore
+{
+    public class CarouselViewModel
+    {
+        public CarouselViewModel()
+        {
+            for (int i = 1; i < 25; i++)
+            {
+                ImageCollection.Add(new CarouselModel("Image" + i + ".jpg"));
+            }
+
+        }
+        private List<CarouselModel> imageCollection = new List<CarouselModel>();
+        public List<CarouselModel> ImageCollection
+        {
+            get { return imageCollection; }
+            set { imageCollection = value; }
+        }
+    }
+}
